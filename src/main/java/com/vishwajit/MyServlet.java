b45a19db326752970493952a70f64f10a37d528d
vishwajit <vishwajitjogalekar@gmail.com>
2021-10-11 01:28:20 +0530

    servlet emamples

---
+++ src/main/java/com/vishwajit/MyServlet.java
@@ -0,0 +1,55 @@
+package com.vishwajit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+//	/**
+//	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		PrintWriter out= response.getWriter();
+//		out.print("This is from service method");
+//	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out= response.getWriter();
+		String name=request.getParameter("fname");
+		out.print("This is from doGet method");
+		out.print("<br> Hello"+name);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out= response.getWriter();
+		String name=request.getParameter("fname");
+		out.print("This is from doPost method");
+		out.print("<br> Hello"+name);
+	}
+
+}
